c443997ef43582f26db68c59397b788304069b7f
Aleksandr Kazakov <a.kazakov@computer-ufa.ru>
2021-08-12 13:29:39 +0400

    update domain name

--- sam_lambda_wg/wg_up/app.py
+++ sam_lambda_wg/wg_up/app.py
@@ -7,7 +7,7 @@ def lambda_handler(event, context):
 
     response = client.create_stack(
         StackName="wireguard",
-        TemplateURL="https://cf-templates-b5s4k2sjkks5-us-east-1.s3.amazonaws.com/2021081Scs-openvpn.yaml",
+        TemplateURL="https://cf-templates-b5s4k2sjkks5-us-east-1.s3.amazonaws.com/2021224YsL-openvpnloozuvpt9he",
         Parameters=[{"ParameterKey": "sshkey", "ParameterValue": "am1"}]
     )
 
